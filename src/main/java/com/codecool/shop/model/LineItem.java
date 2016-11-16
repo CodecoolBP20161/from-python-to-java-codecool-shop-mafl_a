331738b0f46006ad87129fcf7d5b68543511fbfe
b-franka <b.franku@gmail.com>
2016-11-16 12:06:49 +0100

    Implement LineItem class with all fields and methods

--- src/main/java/com/codecool/shop/model/LineItem.java
+++ src/main/java/com/codecool/shop/model/LineItem.java
@@ -1,7 +1,32 @@
 package com.codecool.shop.model;
 
-/**
- * Created by franka on 2016.11.16..
- */
 public class LineItem {
+    private int id;
+    private Product product;
+    private int quantity = 1;
+
+    public int getId() {return this.id; }
+
+    public void setId(int id) {this.id = id;}
+
+    public void setProducts(Product product) {
+        this.product = product;
+    }
+
+    public Product getProducts() {
+        return this.product;
+    }
+
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+
+    public int getQuantity() { return this.quantity; }
+
+    public String toString() {
+        return String.format("id: %1$d, " +
+                        "product: %2$s, " +
+                        "quantity: %3$s, ",
+                this.id,
+                this.product.getName(),
+                String.valueOf(this.quantity));
+    }
 }
