61a50c0e7e37a2221a5769c7c40b132ce79f45a3
b-franka <b.franku@gmail.com>
2016-11-17 10:41:47 +0100

    totalprice, totalquantity, add methods

--- src/main/java/com/codecool/shop/model/Order.java
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -1,25 +1,42 @@
 package com.codecool.shop.model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Order {
-    private int id;
-    private ArrayList<LineItem> lineItems;
 
-    public int getId() {return this.id; }
-
-    public void setId(int id) {this.id = id;}
+    private List<LineItem> lineItems = new ArrayList<>();
 
     public void setLineItems(ArrayList<LineItem> products) {
-        this.lineItems = lineItems;
+        this.lineItems = products;
     }
 
     public ArrayList getLineItems() {
         return this.lineItems;
     }
 
-    public void addLineItem(LineItem lineItem) {
-        this.lineItems.add(lineItem);
+    // returns the total price in cart (lineitem price*quantity)
+    public float getCartTotalPrice() {
+        float total = 0f;
+        for (int i = 0; i < lineItems.size(); i++) {
+            total += lineItems.get(i).getTotalPrice();
+        }
+        return total;
+    }
+
+    // returns number of items in cart
+    public int getTotalQuantity() {
+        int total = 0;
+        for (int i = 0; i < lineItems.size(); i++) {
+            total += lineItems.get(i).getQuantity();
+        }
+        return total;
+    }
+
+    // adds a lineitem with a new id
+    public void addLineItems(LineItem lineItem) {
+        lineItem.setId(lineItems.size() + 1);
+        lineItems.add(lineItem);
     }
 
 }
