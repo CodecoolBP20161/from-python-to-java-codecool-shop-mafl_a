b536e695355a79f751eeaea3888ef8444bab319d
Áron Kékes <kekes.aron@gmail.com>
2016-11-29 14:44:46 +0100

    Refactor tests to be aligned to other test codes and be as optimal as possible

--- src/test/java/com/codecool/shop/dao/ProductCategoryDaoTest.java
+++ src/test/java/com/codecool/shop/dao/ProductCategoryDaoTest.java
@@ -6,6 +6,7 @@ import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -17,23 +18,20 @@ import static org.junit.Assert.*;
  * Created by kekesaron on 2016.11.29..
  */
 public class ProductCategoryDaoTest {
-    ProductCategoryDao productCategoryDao;
-    ProductCategory tablet;
-    ProductCategory smartphone;
-    List<ProductCategory> productCategoryList = new ArrayList<>();
+    static ProductCategoryDao productCategoryDao;
+    static ProductCategory tablet;
+    static ProductCategory smartphone;
+    static List<ProductCategory> productCategoryList = new ArrayList<>();
 
-    @Before
-    public void setUp() {
+    @BeforeClass
+    public static void setUp() {
         productCategoryDao = ProductCategoryDaoMem.getInstance();
         tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
         smartphone = new ProductCategory("Smartphone", "Hardware", "A pocket sized device, thin, flat mobile computer with a touchscreen display, that lets you stay in touch with the world.");
         productCategoryDao.add(tablet);
         productCategoryDao.add(smartphone);
-    }
-
-    @After
-    public void tearDown() {
-        productCategoryDao.getAll().clear();
+        productCategoryList.add(tablet);
+        productCategoryList.add(smartphone);
     }
 
     @Test
@@ -48,8 +46,6 @@ public class ProductCategoryDaoTest {
 
     @Test
     public void testGetAll() {
-        productCategoryList.add(tablet);
-        productCategoryList.add(smartphone);
         assertEquals(productCategoryList, productCategoryDao.getAll());
     }
 
