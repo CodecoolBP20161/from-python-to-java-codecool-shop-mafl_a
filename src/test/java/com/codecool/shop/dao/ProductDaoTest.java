7e08490471814e89ecbb9e9a430f8eaf5ee45df7
b-franka <b.franku@gmail.com>
2016-11-29 13:23:50 +0100

    added testing getall method

--- src/test/java/com/codecool/shop/dao/ProductDaoTest.java
+++ src/test/java/com/codecool/shop/dao/ProductDaoTest.java
@@ -5,9 +5,12 @@ import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
+import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -19,6 +22,8 @@ public class ProductDaoTest {
     Supplier amazon;
     ProductCategory tablet;
     Product product;
+    Product product2;
+    List<Product> productList = new ArrayList<>();
 
     @Before
     public void setUp(){
@@ -26,17 +31,33 @@ public class ProductDaoTest {
         amazon = new Supplier("Amazon", "Digital content and services");
         tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
         product = new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon);
-        productDao.add(product);
+        product2 = new Product("Amazon", 53.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon);
     }
 
     @Test
     public void testAdd(){
+        productDao.add(product);
         assertEquals(1, productDao.getAll().size());
     }
 
     @Test
     public void testFind(){
+        productDao.add(product);
         assertEquals(product, productDao.find(product.getId()));
+    }
+
+    @Test
+    public void testGetAll() {
+        productDao.add(product);
+        productDao.add(product2);
+        productList.add(product);
+        productList.add(product2);
+        assertEquals(productList, productDao.getAll());
+    }
+
+    @After
+    public void after() {
+        productDao.getAll().clear();
     }
 
 
