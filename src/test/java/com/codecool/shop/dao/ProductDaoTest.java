572dab623d928181e7088c31930b05fb2c6d1162
b-franka <b.franku@gmail.com>
2016-11-29 14:17:55 +0100

    refactor

--- src/test/java/com/codecool/shop/dao/ProductDaoTest.java
+++ src/test/java/com/codecool/shop/dao/ProductDaoTest.java
@@ -6,7 +6,6 @@ import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
 import org.junit.After;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -15,9 +14,6 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by kekesaron on 2016.11.29..
- */
 public class ProductDaoTest {
     static ProductDao productDao;
     static Supplier amazon;
@@ -25,7 +21,9 @@ public class ProductDaoTest {
     static ProductCategory smartphone;
     static Product product;
     static Product product2;
-    static List<Product> productList = new ArrayList<>();
+    static List<Product> productListSupplier = new ArrayList<>();
+    static List<Product> productListCategory = new ArrayList<>();
+
 
     @BeforeClass
     public static void setUp(){
@@ -35,50 +33,36 @@ public class ProductDaoTest {
         tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
         product = new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", smartphone, amazon);
         product2 = new Product("Amazon", 53.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon);
-    }
-
-    @After
-    public void tearDown() {
-        productDao.getAll().clear();
-        productList.clear();
+        productDao.add(product);
+        productDao.add(product2);
+        productListSupplier.add(product);
+        productListSupplier.add(product2);
+        productListCategory.add(product);
     }
 
     @Test
     public void testAdd() {
-        productDao.add(product);
-        assertEquals(1, productDao.getAll().size());
+        assertEquals(2, productDao.getAll().size());
     }
 
     @Test
     public void testFind() {
-        productDao.add(product);
         assertEquals(product, productDao.find(product.getId()));
     }
 
     @Test
     public void testGetAll() {
-        productDao.add(product);
-        productDao.add(product2);
-        productList.add(product);
-        productList.add(product2);
-        assertEquals(productList, productDao.getAll());
+        assertEquals(productListSupplier, productDao.getAll());
     }
 
     @Test
     public void testGetBySupplier() {
-        productDao.add(product);
-        productDao.add(product2);
-        productList.add(product);
-        productList.add(product2);
-        assertEquals(productList, productDao.getBy(amazon));
+        assertEquals(productListSupplier, productDao.getBy(amazon));
     }
 
     @Test
     public void testGetByProductCategory() {
-        productDao.add(product);
-        productDao.add(product2);
-        productList.add(product);
-        assertEquals(productList, productDao.getBy(smartphone));
+        assertEquals(productListCategory, productDao.getBy(smartphone));
     }
 
 }
