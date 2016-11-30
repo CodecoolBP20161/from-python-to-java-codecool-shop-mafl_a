d048ab6adda9967dc88a38cb65932f7a7eb00b2a
Áron Kékes <kekes.aron@gmail.com>
2016-11-30 12:06:27 +0100

    Implement add method

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoJDBC.java
@@ -1,5 +1,73 @@
 package com.codecool.shop.dao.implementation;
 
 
-public class ProductDaoJDBC {
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+public class ProductDaoJDBC implements ProductDao{
+
+    private static final String DATABASE = "jdbc:postgresql://localhost:5432/codecoolshop";
+    private static final String DB_USER = "postgres";
+    private static final String DB_PASSWORD = "postgres";
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                DATABASE,
+                DB_USER,
+                DB_PASSWORD);
+    }
+
+    @Override
+    public void add(Product product) {
+        String query = "INSERT INTO todos (" +
+                "name, description, default_price, currency, product_category, supplier) " +
+                "VALUES ('" + product.getName() + "', '" + product.getDescription() + "', '"
+                + product.getPrice() + "', '" + product.getProductCategory() + "', '"
+                + product.getSupplier() + "');";
+        executeQuery(query);
+    }
+
+    @Override
+    public Product find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Product> getBy(Supplier supplier) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getBy(ProductCategory productCategory) {
+        return null;
+    }
+
+    private void executeQuery(String query) {
+        try (Connection connection = getConnection();
+             Statement statement =connection.createStatement();
+        ){
+            statement.execute(query);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
