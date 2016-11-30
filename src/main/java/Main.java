cfd3b6ca6bf4c2598393cf08f8121254dcfdf21e
b-franka <b.franku@gmail.com>
2016-11-30 22:24:28 +0100

    changed all memory stores to db

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ import com.codecool.shop.dao.*;
 import com.codecool.shop.dao.implementation.Mem.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.Mem.ProductDaoMem;
 import com.codecool.shop.dao.implementation.Mem.SupplierDaoMem;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoJDBC;
 import com.codecool.shop.dao.implementation.ProductDaoJDBC;
 import com.codecool.shop.dao.implementation.SupplierDaoJDBC;
 import com.codecool.shop.model.*;
@@ -47,7 +48,7 @@ public class Main {
     public static void populateData() {
 
         ProductDao productDataStore = ProductDaoJDBC.getInstance();
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoJDBC.getInstance();
         SupplierDao supplierDataStore = SupplierDaoJDBC.getInstance();
 
         //setting up a new supplier
@@ -70,9 +71,9 @@ public class Main {
         //setting up products and printing it
         productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
-        productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
-        productDataStore.add(new Product("Samsung Galaxy Note 7", 899, "USD", "Samsung's top-of-the-line smartphone. This product is a total BLAST!", smartphone, samsung));
-        productDataStore.add(new Product("Apple iPhone 6 Plus Space Gray 64GB", 529.99f, "USD", "Apple's best smartphone, with 64GB of storage and a 3.5mm jack port.", smartphone, apple));
+        productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon''s latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
+        productDataStore.add(new Product("Samsung Galaxy Note 7", 899, "USD", "Samsung''s top-of-the-line smartphone. This product is a total BLAST!", smartphone, samsung));
+        productDataStore.add(new Product("Apple iPhone 6 Plus Space Gray 64GB", 529.99f, "USD", "Apple''s best smartphone, with 64GB of storage and a 3.5mm jack port.", smartphone, apple));
     }
 
 
