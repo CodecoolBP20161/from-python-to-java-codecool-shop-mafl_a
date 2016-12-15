38168c1015aa31f1ebeb57496db6786c5013ffb6
b-franka <b.franku@gmail.com>
2016-12-15 14:11:38 +0100

    some field to order modell

--- src/main/java/com/codecool/shop/model/Order.java
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -1,20 +1,43 @@
 package com.codecool.shop.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class Order {
 
+    @Getter @Setter
+    private int id;
+
+    @Getter @Setter
+    private String firstName;
+
+    @Getter @Setter
+    private String lastName;
+
+    @Getter @Setter
+    private String email;
+
+    @Getter @Setter
+    private String phoneNumber;
+
+    @Getter @Setter
+    private String address;
+
+    @Getter @Setter
+    private String city;
+
+    @Getter @Setter
+    private String country;
+
+    @Getter @Setter
+    private String zipCode;
+
+    @Getter @Setter
     private List<LineItem> lineItems = new ArrayList<>();
-
-//    public void setLineItems(List<LineItem> products) {
-//        this.lineItems = products;
-//    }
-
-    public List getLineItems() {
-        return this.lineItems;
-    }
 
     // returns the total price in cart (lineitem price*quantity)
     public float getCartTotalPrice() {
