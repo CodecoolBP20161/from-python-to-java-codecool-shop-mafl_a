8cacb36f605d16355a9c747273945033fe67f278
b-franka <b.franku@gmail.com>
2017-01-24 10:49:56 +0100

    added the method that return the list with embed codes

--- src/main/java/com/codecool/shop/controller/VideoServiceController.java
+++ src/main/java/com/codecool/shop/controller/VideoServiceController.java
@@ -1,7 +1,22 @@
 package com.codecool.shop.controller;
 
-/**
- * Created by franka on 2017.01.24..
- */
+import org.json.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class VideoServiceController {
+
+    public List getProductVideos(String apiJsonAsString) {
+        List<String> embedCodes = new ArrayList<>();
+
+        JSONArray jsonArray = new JSONArray(apiJsonAsString);
+        JSONObject unboxingJson = new JSONObject(jsonArray.get(0));
+        JSONObject reviewJson = new JSONObject(jsonArray.get(2));
+
+        embedCodes.add(unboxingJson.getString("embed code"));
+        embedCodes.add(reviewJson.getString("embed code"));
+        return embedCodes;
+    }
+
 }
