eeda7742ae8a44520354097d368c234bf8053f8f
Áron Kékes <kekes.aron@gmail.com>
2017-01-24 10:39:20 +0100

    Add get route for the individual product pages

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -37,6 +37,9 @@ public class Main {
         // Always add generic routes to the end
         get("/", ProductController::renderAll, new ThymeleafTemplateEngine());
 
+        //INSERT METHOD HERE
+        get("/product", ProductController::renderProductPage, new ThymeleafTemplateEngine());
+
         // Add this line to your project to enable the debug screen
         enableDebugScreen();
     }
