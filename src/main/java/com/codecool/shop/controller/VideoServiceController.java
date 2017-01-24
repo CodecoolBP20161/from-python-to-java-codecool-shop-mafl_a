7297f9a2438cbe16810c22aa988a081a3003d045
wH47wH0 <csepella.miki@gmail.com>
2017-01-24 11:28:52 +0100

    add execute() method in VideoServiceController

--- src/main/java/com/codecool/shop/controller/VideoServiceController.java
+++ src/main/java/com/codecool/shop/controller/VideoServiceController.java
@@ -1,7 +1,18 @@
 package com.codecool.shop.controller;
 
+import org.apache.http.client.fluent.Request;
+
+import java.io.IOException;
+import java.net.URI;
+
 /**
  * Created by franka on 2017.01.24..
  */
 public class VideoServiceController {
+    private String execute(URI uri) throws IOException {
+        return Request.Get(uri)
+                .execute()
+                .returnContent()
+                .asString();
+    }
 }
